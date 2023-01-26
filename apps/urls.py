220b3e298eac91ff8b047e034e34539526245bf8
HyeonsooKim <gustn9442@naver.com>
2023-01-27 08:27:41 +0900

    feat: 집계 추가

--- apps/urls.py
+++ apps/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('users/', include('apps.users.urls')),
     path('boards/', include('apps.boards.urls')),
+    path('statistic/', include('apps.statistic.urls')),
 ]
